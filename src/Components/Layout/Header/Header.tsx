d6f5081b296761def1a0f2163efdff413a1371e6
Sergio Balaguer Carmona <sbalaguercarmona@gmail.com>
2023-07-08 17:34:58 +0200

    Apply some styling on header

--- src/Components/Layout/Header/Header.tsx
+++ src/Components/Layout/Header/Header.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import './Header.css';
+import RakutenLogo from "../../RakutenLogo/RakutenLogo";
+
+
 
 export default () => {
     return <header className="layout-header">
-        <div>Header content</div>
+        <div className="w-100">
+            <RakutenLogo className="position-abolute" fillColor="#000000"/>
+            <div className="header-links">
+                {getHeaderLink({ title: "Inicio", url: "https://www.rakuten.tv/es/" })}
+                {getHeaderLink({ title: "Tienda", url: "https://www.rakuten.tv/es/gardens/store" })}
+                {getHeaderLink({ title: "Gratis", url: "https://www.rakuten.tv/es/gardens/free" })}
+                {getHeaderLink({ title: "Canales TV", url: "https://www.rakuten.tv/es/live_channels" })}
+            </div>
+        </div>
+        <div className="w-100 float-right">
+        <span>
+            <strong>Iniciar Sesión</strong> o <strong>Registrarse</strong>
+        </span>
+        </div>
     </header>;
 }   
+
+const getHeaderLink = (args: { title: string, url: string }) => {
+    return <span className="m-1">
+        <strong>
+            <a href={args.url}>{args.title}</a>
+        </strong>
+    </span>
+}
